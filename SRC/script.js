06ca75ad2b9bdc26180b18d4685e4ba65e2a7036
mayabaandiswa-eng <mayabaandiswa@gmail.com>
2026-06-12 09:04:40 +0200

    Add search form submission handler

--- SRC/script.js
+++ SRC/script.js
@@ -1,3 +1,12 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
